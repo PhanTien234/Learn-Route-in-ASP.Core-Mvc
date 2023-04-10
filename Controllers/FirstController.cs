57e05629797dfec6c444d1e8096ac19b3a596993
PhanTien234 <minhtienp328@gmail.com>
2023-04-10 17:09:04 +0700

    use IActionResult by ContentResult

--- Controllers/FirstController.cs
+++ Controllers/FirstController.cs
@@ -4,7 +4,10 @@ namespace App.Controllers
 {
     public class FirstController : Controller
     {
-
+        private readonly ILogger<FirstController> _logger;
+        public FirstController(ILogger<FirstController> logger){
+            _logger = logger;
+        }
         public string Index()
         {
             //this.HttpContext
@@ -18,7 +21,51 @@ namespace App.Controllers
             //this.ViewBag
             //this.Url
 
+            // _logger.Log(LogLevel.Warning, "THong bao abc");
+            _logger.LogWarning("Thong bao");
+            _logger.LogError("Thong bao");
+            _logger.LogDebug("Thong bao");
+            _logger.LogCritical("Thong bao");
+            _logger.LogInformation("Index Action");
+            //Serilog
+
+            // Console.WriteLine("Index Action");
             return "Toi la Index cua fisrt";
         }
+
+        public void Nothing(){
+            _logger.LogInformation("Nothing Action");
+            Response.Headers.Add("hi", "xin chao cac ban");
+        }
+
+        public object Anything() => new int[] {1, 2, 3};
+        public IActionResult Readme(){
+            var content = @"
+                Xin chao cac ban,
+                cac ban dang hoc ve ASP.NET MVC
+
+
+                XUANTHULAB.NET
+            ";
+            return Content(content, "text/plain");
+        }
+
+    //     Kiểu trả về                 | Phương thức
+    //  IActionResult
+    // ------------------------------------------------
+    // ContentResult               | Content()
+    // EmptyResult                 | new EmptyResult()
+    // FileResult                  | File()
+    // ForbidResult                | Forbid()
+    // JsonResult                  | Json()
+    // LocalRedirectResult         | LocalRedirect()
+    // RedirectResult              | Redirect()
+    // RedirectToActionResult      | RedirectToAction()
+    // RedirectToPageResult        | RedirectToRoute()
+    // RedirectToRouteResult       | RedirectToPage()
+    // PartialViewResult           | PartialView()
+    // ViewComponentResult         | ViewComponent()
+    // StatusCodeResult            | StatusCode()
+    // ViewResult                  | View()
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+// builder.Services.AddTransient(typeof(ILogger<>), typeof(Logger<>)); Serilog
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
